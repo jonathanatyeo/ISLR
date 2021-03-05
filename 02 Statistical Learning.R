dd3e3aadb83b21917b211faba53ada5a7145d998
Jonathan <jonathan.atyeo@gmail.com>
2021-03-05 07:02:48 +0000

    Updated 02 Statistical Learning

--- 02 Statistical Learning.R
+++ 02 Statistical Learning.R
@@ -72,7 +72,17 @@ ggplot(data = Auto, mapping = aes(x = displacement, y = mpg)) +
 
 # horsepower and weight are positively correlated
 ggplot(data = Auto, aes(x = horsepower, y = weight)) +
-  geom_point(aes(colour = year)) +
+  geom_point(aes(colour = factor(origin))) +
+  geom_smooth()
+
+# Inverse relationship between mpg and horsepower
+ggplot(data = Auto, mapping = aes(x = horsepower, y = mpg)) +
+  geom_point() +
+  geom_smooth()
+
+# Positive correlation between mpg and horsepower
+ggplot(data = Auto, mapping = aes(x = horsepower, y = displacement)) +
+  geom_point() +
   geom_smooth()
 
 # There is one outlier with high horsepower for a lower weight
@@ -82,3 +92,82 @@ Auto %>%
 # (f) Suppose that we wish to predict gas mileage (mpg) on the basis of the other 
 # variables. Do your plots suggest that any of the other variables might be useful
 # in predicting mpg? Justify your answer.
+
+# There is an inverse relationship between mpg and displacement and mpg and
+# horsepower.
+
+# Exercise 10 -------------------------------------------------------
+
+# This exercise involves the Boston housing data set.
+
+# (a) To begin, load in the Boston data set. The Boston data set is part of the 
+# MASS library in R.
+library(MASS)
+
+# Now the data set is contained in the object Boston.
+Boston
+
+# Read about the data set:
+?Boston
+
+# How many rows are in this data set? How many columns? What do the rows and 
+# columns represent?
+
+# There are 506 rows and 14 columns.
+# Each row represents a suburb of Boston
+# The columns represent various demographics including the median value of owner-
+# occupied homes in $1000s (medv)
+
+# (b) Make some pairwise scatterplots of the predictors (columns) in this data set.
+# Describe your findings.
+pairs(Boston[, c(1:5, 14)])
+pairs(Boston[, c(6:9, 14)])
+pairs(Boston[, c(10:13, 14)])
+
+# There appears to be a positive relationship between medv (median value of owner-
+# occupied homes in $1000s) and rm (average number of rooms per dwelling) and 
+# dis (weighted mean of distances to five Boston employment centres).
+
+ggplot(Boston, aes(x = rm, y = medv)) +
+  geom_point()
+
+ggplot(Boston, aes(x = dis, y = medv)) +
+  geom_point()
+
+# There appears to be an inverse relationship between medv and nox (nitrogen oxide 
+# concentration) and lstat(lower status of the population). 
+
+ggplot(Boston, aes(x = nox, y = medv)) +
+  geom_point()
+
+ggplot(Boston, aes(x = lstat, y = medv)) +
+  geom_point()
+
+# Other relationships appear to be between nox and indus (proportion of residential
+# land zoned for lots over 25,000 sq.ft.), nox and dis, and lstat and rm.
+
+ggplot(Boston, aes(x = nox, y = indus)) +
+  geom_point()
+
+ggplot(Boston, aes(x = nox, y = dis)) +
+  geom_point()
+
+ggplot(Boston, aes(x = rm, y = lstat)) +
+  geom_point()
+
+# (c) Are any of the predictors associated with per capita crime rate? If so, 
+# explain the relationship.
+
+# (d) Do any of the suburbs of Boston appear to have particularly high crime rates?
+# Tax rates? Pupil-teacher ratios? Comment on the range of each predictor.
+
+# (e) How many of the suburbs in this data set bound the Charles river? What is the
+# median pupil-teacher ratio among the towns in this data set?
+
+# (g) Which suburb of Boston has lowest median value of owneroccupied homes? What 
+# are the values of the other predictors for that suburb, and how do those values 
+# compare to the overall ranges for those predictors? Comment on your findings.
+
+# (h) In this data set, how many of the suburbs average more than seven rooms per 
+# dwelling? More than eight rooms per dwelling? Comment on the suburbs that average
+# more than eight rooms per dwelling.
